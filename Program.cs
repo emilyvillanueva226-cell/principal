c596cf7e4a4a6365b69c074f84c2d17da8ca2a71
emilyvillanueva226-cell <emilyvillanueva226@gmail.com>
2025-10-07 10:53:07 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int a, b, c;
+            Console.Write("Ingresar numero 1: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Ingresar numero 2: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Ingresar numero 3: ");
+            c = int.Parse(Console.ReadLine());
+            int mayor = ejercicio4.buscarMayor(a, b, c);
+            int menor = ejercicio4.buscarMenor(a, b, c);
+            int inter = ejercicio4.buscarIntermedio(a, b, c, mayor, menor);
+            Console.WriteLine("El mayor es = {0}", mayor);
+            Console.WriteLine("El menor es = {0}", menor);
+            Console.WriteLine("El intermedio es = {0}", inter);
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+    
+
---
+++ ejercicio4.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio4
+{
+    internal class ejercicio4
+    {
+
+       public  static int buscarMayor(int a, int b, int c)
+        {
+            int mayor;
+            if (a > b && a > c)
+            {
+                mayor = a;
+            }
+            else
+            {
+                if (b > c)
+                {
+                    mayor = b;
+                }
+                else
+                {
+                    mayor = c;
+                }
+            }
+            return mayor;
+        }
+        public static int buscarMenor(int a, int b, int c)
+        {
+            int menor;
+            if (a <= b && a <= c)
+            {
+                menor = a;
+            }
+            else
+            {
+                if (b < c)
+                {
+                    menor = b;
+                }
+                else
+                {
+                    menor = c;
+                }
+            }
+            return menor;
+        }
+
+        public static int buscarIntermedio(int a, int b, int c, int mayor,int menor)
+        {
+            int inter;
+            inter = (a + b + c) - (mayor + menor);
+            return inter;
+        }
+        
+    }
+}
+
+    
+    
